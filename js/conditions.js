23a5f0d38391300a4ad9b9b923696cc798d165a7
Juan Meissner <jc_meissner@hotmail.com>
2026-05-19 20:57:39 -0300

    Organização de código

---
+++ js/conditions.js
@@ -0,0 +1,62 @@
+const conditionDescriptions = {
+
+    '😍': {
+        title: 'Enfeitiçado',
+        desc: 'A criatura fica encantada e nÃ£o pode atacar quem a enfeitiçou.'
+    },
+    
+    '😱': {
+        title: 'Medo',
+        desc: 'Movimento reduzido pela metade, se falhar em teste de Coragem irá ficar paralisado durante o turno inteiro.'
+    },
+    
+    '🚫': {
+        title: 'Incapacitado',
+        desc: 'O alvo ficará desmaiado até passar em um teste de resistência ND 18.'
+    },
+    
+    '👻': {
+        title: 'Invisível',
+        desc: 'Díficil de detectar visualmente e recebe vantagens apropriadas.'
+    },
+    
+    '🐍': {
+        title: 'Envenenado',
+        desc: 'Sofre 1d6 de dano por turno e –2 em testes físicos. Pode fazer um teste de resistência ao fim de cada turno para encerrar o efeito.												'
+    },
+    
+    '🧎': {
+        title: 'Caí­do',
+        desc: 'Sofre desvantagem em ataques corpo a corpo, ataques contra ele têm vantagem, e levantar-se consome uma ação, pode esquivar ou bloquear.'
+    },
+    
+    '😮': {
+        title: 'Exausto',
+        desc: 'A criatura sofre penalidades física progressivas.'
+    },
+    
+    '🔥': {
+        title: 'Em Chamas',
+        desc: 'Sofre 1d6 de dano por turno até apagar o fogo (ação completa ou teste apropriado). Testes físicos sofrem –2 enquanto estiver em chamas.'
+    },
+    
+    '💫': {
+        title: 'Atordoado',
+        desc: 'O personagem perde a próxima ação e não pode realizar defesa ou esquiva até o fim do próximo turno.'
+    },
+    
+    '🩸': {
+        title: 'Sangrando',
+        desc: 'Sofre 1d6 de dano por turno até receber primeiros socorros ou ser curado. O efeito acumula se reaplicado.'
+    },
+    
+    '🧊': {
+        title: 'Congelado',
+        desc: 'Movimento reduzido pela metade e desvantagem em ações físicas. Se falhar em um teste de resistência, fica Atordoado por 1 turno.'
+    },
+    
+    '⚖️': {
+        title: 'Desequilibrado',
+        desc: 'Sofre –2 em ataques e defesas e não pode realizar reações até o próximo turno.'
+    }
+    };
